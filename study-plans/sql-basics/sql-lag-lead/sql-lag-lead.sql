5ffe61de177e3482f4472ac3558d5519563c2b38
hadyelazizy <hadyelazizy@gmail.com>
2026-05-04 00:05:47 +0300

    Add solution for sql-lag-lead

---
+++ study-plans/sql-basics/sql-lag-lead/sql-lag-lead.sql
@@ -0,0 +1,5 @@
+-- Write your SQL query here
+select month,revenue,LAG(revenue,1,0) OVER (order by month) as prev_revenue,
+revenue - prev_revenue as revenue_change
+from monthly_revenue 
+order by month asc;
